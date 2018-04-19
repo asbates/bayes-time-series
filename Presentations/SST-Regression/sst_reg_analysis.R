a058e9ac923667742e33fd5278fef38471ec4f76
Andrew Bates <andrewbates73@gmail.com>
2018-04-19 09:31:29 -0700

    add initial plots to presentation

--- Presentations/SST-Regression/sst_reg_analysis.R
+++ Presentations/SST-Regression/sst_reg_analysis.R
@@ -2,14 +2,20 @@
 library(readr)
 library(bsts)
 
-gibraltar <- read_csv("data/gilbralter_time_series_r.csv", 
-                       col_types = cols(startDate = col_date(format = "%Y-%m-%d"), 
+gib <- read_csv("data/gilbralter_time_series_r.csv", 
+                       col_types = cols(startDate = col_skip(), 
                                         timeIdx = col_skip()))
+names(gib) <- c('SST', '10', '20', '30', '40',
+                      '50', '60', '70', '80', '90')
+gib <- zooreg(gibraltar, start = c(2004, 1, 1), end = c(2017, 11, 29),
+               frequency = 12)
 
-g <- data.frame(gibraltar)
-rownames(g) <- g$startDate
-gilt <- ts(g$X0, start = c(2004, 1, 1), end = c(2017, 11, 29), frequency = 12)
-plot(gilt, main='SST of Gilbralter region',
+plot(gib$SST, main='SST of Gilbralter region',
      xlab='date',
      ylab='Temperature [C]', 
      ylim=c(15, 26))
+
+ss <- AddLocalLinearTrend(list(), gib$SST)
+ss <- AddSeasonal(ss, gib$SST, nseasons = 12)
+model1 <- bsts(SST ~., state.specification = ss,
+               data = gib, niter = 1000)
